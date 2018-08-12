3e3e716e9513c58a0686f2f5ef7582c059f15c50
Sheena Bonilla <sheena.myb@gmail.com>
2018-08-12 14:07:53 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ attr_accessor :discount, :total
   end
   
   def add_item(title, price, quantity = 1)
-    @basket << title * quantity
+    @basket << title
     self.total += price * quantity
     
   end
